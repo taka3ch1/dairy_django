ab432e010a1d819989604a395efd6a64b91a273b
taka3ch1 <takahashikouki@Kmac.local>
2022-05-30 23:26:05 +0900

    Chapter 10 list10.9

--- diary/forms.py
+++ diary/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.mail import EmailMessage
+import os
 
 class InquiryForm(forms.Form):
     name = forms.CharField(label="お名前",max_length=30)
@@ -16,3 +18,23 @@ class InquiryForm(forms.Form):
         self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください'
         self.fields['message'].widget.attrs['class'] = 'form-control'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください'
+    
+
+
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+        
+        subject = 'お問合せ{}'.format(title)
+        message = '送信者名: {0}¥nメールアドレス:{1}¥nメッセージ:¥n{2}'.format(name,email,message)
+        from_email = os.environ.get('FROM_EMAIL')
+        to_list=[
+            os.environ.get('From_EMAIL')
+        ]
+        cc_list=[
+            email
+        ]
+        message = EmailMessage(subject=subject, body=message,from_email=from_email,to=to_list,cc=cc_list)
+        message.send()
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,5 @@
+from tabnanny import verbose
 from django.db import models
+
 
 # Create your models here.
